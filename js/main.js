eabe3b9c83c2ee69e75ab3dc1e59e7d2d1f2c841
Contrary Sheep <contrarysheep@contrarysheep.local>
2013-09-04 14:05:11 -0400

    update javascript for select menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+$(document).ready(mobileMenuWatcher)
+
 function toggleMenu(element){
   if(element == "#tags-dropdown"){
     $('#category-dropdown').hide();
@@ -5,4 +7,10 @@ function toggleMenu(element){
     $('#tags-dropdown').hide();
   }
   $(element).toggle();
+}
+
+function mobileMenuWatcher() {
+  $('.more_phone_select_menu').change(function() {
+    window.location.href = $(this).val();
+  });
 }
